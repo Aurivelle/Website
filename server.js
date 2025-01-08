ef9134056644814340038bb47d66232a764101fa
Yu <jianao931212@gmail.com>
2025-01-08 20:31:32 +0800

    108

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(
   cors({
-    origin: "https://website-seven-omega-42.vercel.app",
+    origin: "*",
   })
 );
 app.use(express.json());
